4bd6c01a5616ff0370e6eddef8eca6722a2013b6
Will Marsh <wtmrsh@gmail.com>
2024-02-29 18:34:49 +0000

    tidy up

--- RebuildAll.bat
+++ RebuildAll.bat
@@ -1,2 +1,2 @@
-devenv build\vs2017\Utils.sln /Rebuild "Release|x86"
-devenv build\vs2017\Utils.sln /Rebuild "Debug|x86"
+devenv build\vs2017\Utils.sln /Rebuild "Release|Win32"
+devenv build\vs2017\Utils.sln /Rebuild "Debug|Win32"
