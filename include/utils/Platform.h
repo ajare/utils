66a19ee2391df145d93343bfa602e189785bb111
Will Marsh <wtmrsh@gmail.com>
2024-03-05 20:36:50 +0000

    change platform defines

--- include/utils/Platform.h
+++ include/utils/Platform.h
@@ -1,47 +1,47 @@
 #pragma once
 
 // Platform settings - based off OGRE3D (www.ogre3d.org)
-#define WP_PLATFORM_WINDOWS 1
-#define WP_PLATFORM_LINUX 2
-#define WP_PLATFORM_APPLE 3
+#define UTILS_PLATFORM_WINDOWS 1
+#define UTILS_PLATFORM_LINUX 2
+#define UTILS_PLATFORM_APPLE 3
 
-#define WP_COMPILER_MSVC 1
-#define WP_COMPILER_GNUC 2
-#define WP_COMPILER_BORL 3
+#define UTILS_COMPILER_MSVC 1
+#define UTILS_COMPILER_GNUC 2
+#define UTILS_COMPILER_BORL 3
 
 // Find compiler information
 #if defined( _MSC_VER )
-#   define WP_COMPILER WP_COMPILER_MSVC
-#   define WP_COMP_VER _MSC_VER
+#   define UTILS_COMPILER UTILS_COMPILER_MSVC
+#   define UTILS_COMP_VER _MSC_VER
 #elif defined( __GNUC__ )
-#   define WP_COMPILER WP_COMPILER_GNUC
-#   define WP_COMP_VER (((__GNUC__)*100) + \
+#   define UTILS_COMPILER UTILS_COMPILER_GNUC
+#   define UTILS_COMP_VER (((__GNUC__)*100) + \
 	(__GNUC_MINOR__ * 10) + \
 	__GNUC_PATCHLEVEL__)
 #elif defined( __BORLANDC__ )
-#   define WP_COMPILER WP_COMPILER_BORL
-#   define WP_COMP_VER __BCPLUSPLUS__
+#   define UTILS_COMPILER UTILS_COMPILER_BORL
+#   define UTILS_COMP_VER __BCPLUSPLUS__
 #else
 #   pragma error "Unknown compiler."
 #endif
 
 // Set platform
 #if defined( _WIN32 )
-#   define WP_PLATFORM WP_PLATFORM_WINDOWS
+#   define UTILS_PLATFORM UTILS_PLATFORM_WINDOWS
 #elif defined( __APPLE_CC__)
-#   define WP_PLATFORM WP_PLATFORM_APPLE
+#   define UTILS_PLATFORM UTILS_PLATFORM_APPLE
 #else
-#   define WP_PLATFORM WP_PLATFORM_LINUX
+#   define UTILS_PLATFORM UTILS_PLATFORM_LINUX
 #endif
 
 // DLL Export
-#if WP_PLATFORM == WP_PLATFORM_WINDOWS
+#if UTILS_PLATFORM == UTILS_PLATFORM_WINDOWS
 #	if defined(UTILS_DLL_EXPORT)
 #		define UTILS_API __declspec( dllexport )
 #	else
 #		define UTILS_API
 #	endif
-#elif WP_PLATFORM == WP_PLATFORM_LINUX
+#elif UTILS_PLATFORM == UTILS_PLATFORM_LINUX
 #	if defined(UTILS_DLL_EXPORT)
 #		define UTILS_API __attribute__((visibility("default")))
 #	else
@@ -50,7 +50,7 @@
 #endif
 
 // Ok, because only occurs on non-public STL members
-#if WP_PLATFORM == WP_PLATFORM_WINDOWS
+#if UTILS_PLATFORM == UTILS_PLATFORM_WINDOWS
 #	pragma warning(disable: 4251)
 #endif
 
